f1bd9f9ef5ca828cd1da681304edd03d43fcf947
0spring0 <coffee_and_greentea@outlook.jp>
2021-01-18 12:37:51 +0900

    VR機能の追加 敵の追加

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody _rigidbody;
+    [SerializeField]
+    private SphereCollider _mycollider;
+    [SerializeField]
+    private float _speed;
+    private void Start()
+    {
+        Vector3 direction = new Vector3(Random.value * 2.0F - 1.0F, Random.value * 2.0F - 1.0F, Random.value * 2.0F - 1.0F) * _speed;
+        _rigidbody.AddForce(direction, ForceMode.Impulse);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+            Destroy(gameObject);
+        if (other.gameObject.tag == "Wall")
+            _mycollider.isTrigger = false;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Wall")
+            _mycollider.isTrigger = true;
+    }
+}
--- Assets/Script/Racket.cs
+++ Assets/Script/Racket.cs
@@ -12,6 +12,9 @@ public class Racket : MonoBehaviour
     private float _speed;
     private Vector3 _late_position;
 
+    [SerializeField]
+    private GameObject _prefub;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,39 +22,20 @@ public class Racket : MonoBehaviour
         _late_position = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        Vector3 direction = transform.position - _late_position;
         _late_position = transform.position;
-        RaycastHit info;
-        bool col = false;
-        col = Physics.BoxCast(transform.position + _box_collider.center, _box_collider.size * 0.5F, -direction, out info,transform.rotation,direction.magnitude);
-        Debug.Log(transform.position + _box_collider.center);
-        //col = Physics.Raycast(transform.position, -direction.normalized, out info, direction.magnitude);
-        //Debug.box(transform.position, direction, Color.red);
-        Gizmos.DrawCube(transform.position + _box_collider.center, _box_collider.size);
-        if (col == false)
-            return;
-        if (info.transform.tag == "Ball")
+
+        if (Input.GetKeyDown(KeyCode.Return))
+            Instantiate(_prefub);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //ボールをラケットの向いている方向に振った速度に応じて力を与える
+        if (collision.gameObject.tag == "Ball")
         {
-            info.rigidbody.AddForce(direction * _speed, ForceMode.Impulse);
+            collision.rigidbody.AddForce(-(_late_position - transform.position) * _speed,ForceMode.Impulse);
         }
     }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.matrix = transform.localToWorldMatrix;
-        Gizmos.DrawRay(transform.position + _box_collider.center, new Vector3(0.0F, -1.0F, 0.0F));//(transform.position - _late_position));
-    }
-
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    //ボールをラケットの向いている方向に振った速度に応じて力を与える
-    //    if(collision.gameObject.tag == "Ball")
-    //    {
-    //        collision.rigidbody.AddForce(-collision.rigidbody.velocity, ForceMode.Impulse);
-    //        //collision.rigidbody.AddForce(-(_late_position - transform.position) * _speed,ForceMode.Impulse);
-    //    }
-    //}
 }
